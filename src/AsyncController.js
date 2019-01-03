42a9c0149a272bb489572986f6c4f20bac3fbbd3
Hefeng Wang <jameswhf@qq.com>
2019-01-03 22:31:19 +0800

    enhancement: 支持delay

--- src/AsyncController.js
+++ src/AsyncController.js
@@ -1,4 +1,3 @@
-
 const STATUS = {
     WAITING: 1,
     EXECUTING: 2,
@@ -9,32 +8,56 @@ const STATUS = {
 
 const QUEUE_LIMIT = 5
 
-const isAborted = task => task.status === STATUS.ABORTED
-const canAbort = task => task.status === STATUS.EXECUTING || STATUS.WAITING
+const canAbort = task => [ STATUS.EXECUTING, STATUS.WAITING ].includes(task.status)
+
+function makeDelay (delay) {
+    return new Promise((resolve) => setTimeout(resolve, delay))
+}
+function abortCallback (task, callback) {
+    return (res) => {
+        if (canAbort(task) && callback) {
+            return callback(res)
+        }
+        return task
+    }
+}
+
+function abortify (task, genPro, resolve, reject, options = {}) {
+    const { delay = 0 } = options
+    const abortThen = abortCallback(task, (res) => {
+        task.status = STATUS.FINISHED
+        return resolve && resolve(res)
+    })
+    const abortCatch = abortCallback(task, (res) => {
+        task.status = STATUS.FAILED
+        return reject && reject(res)
+    })
+    const getAbPro = () => genPro().then(abortThen).catch(abortCatch)
+    if (delay <= 0) return getAbPro()
+    return makeDelay(delay).then(abortCallback(task, () => getAbPro()))
+}
 
 
 class AsyncController {
     tasks = []
-    register (spec, pro, resolve, reject) {
-        const task = { ...spec, status: STATUS.EXECUTING }
-        task.pro = pro.then(res => {
-            if (!isAborted(task) && resolve) {
-                task.status = STATUS.FINISHED
-                return resolve(res)
-            }
-        }).catch(err => {
-            if (!isAborted(task) && reject) {
-                task.status = STATUS.FAILED
-                return reject(err)
-            }
-        })
+    reqNum = 0
+    register (spec, genPro, resolve, reject, options) {
+        const task = { ...spec, status: STATUS.EXECUTING, reqNum: this.reqNum }
+        task.pro = abortify(task, genPro, resolve, reject, options)
         this.tasks.push(task)
         if (this.tasks.length >= QUEUE_LIMIT) {
             this.clearIdleTask()
         }
+        this.reqNum += 1
+        return task
     }
     abort (matchFunc) {
-        this.tasks.filter(canAbort).filter(matchFunc).forEach(task => { task.status = STATUS.ABORTED })
+        this.tasks.filter(canAbort).filter(matchFunc).forEach(task => {
+            task.status = STATUS.ABORTED
+            if (task.cancel) {
+                task.cancel()
+            }
+        })
         this.clearIdleTask()
     }
     abortAll () {
@@ -43,9 +66,12 @@ class AsyncController {
     clearIdleTask () {
         this.tasks = this.tasks.filter(canAbort)
     }
+    getTask (matchFunc) {
+        return this.tasks.filter(canAbort).find(matchFunc)
+    }
 }
 
-function mixinAsyncController (component) {
+export function mixinAsyncController (component) {
     const controller = new AsyncController()
     if (!component.componentWillUnmount) {
         component.componentWillUnmount = () => controller.abortAll()
